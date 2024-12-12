57907f84fac18d0e94a6affce97db49a6ec45ce3
Anthony Alexander Lozada Requelme <174895605+AnthonyLozadaAlexander@users.noreply.github.com>
2024-12-12 18:27:38 -0500

    update

--- ejercicio_While.py
+++ ejercicio_While.py
@@ -3,9 +3,11 @@ print("------------")
 PI = 3.1416
 
 radio = int(input("Ingresar el radio: "))
-while (radio <= 1):
+while (radio < 1):
+  
   print("-------------------------------------")
-  print("Debe ingresar un valor del radio > 1")
+  print("El radio debe ingresar un valor >= 1")
   radio = int(input("Ingrese un radio: "))
 
 area = PI * radio ** 2
+print(f"El area usando el radio del circulo es: {area}")
